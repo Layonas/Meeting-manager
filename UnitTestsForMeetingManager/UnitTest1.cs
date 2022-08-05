75ae726a7c0d2cc8bc1eab686cd6cc0f42b3c9e6
Layonas <dovdaszl@gmail.com>
2022-08-05 16:43:59 +0300

    Initial commit

---
+++ UnitTestsForMeetingManager/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System.Linq;
+using Xunit;
+using System;
+
+namespace UnitTestsForMeetingManager
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
